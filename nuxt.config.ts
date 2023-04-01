b5ff3ee59ebd297d24d33ef2da22c1a81f3fad10
DanCuatro <alucard11096@gmail.com>
2023-03-31 21:04:15 -0600

    UI-Auth init

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  extends: [
+    '@clau-org/ui-core'
+  ],
+  srcDir: 'src/',
+  nitro: {
+    preset:'deno'
+  }
+})
---
+++ tests/hello.test.ts
@@ -0,0 +1,11 @@
+import { describe, test,     } from 'vitest'
+
+describe('Test Empty', async () => {
+  describe('Empty Test', async () => {
+    test('Hello empty', ({ expect }) => {
+      return true
+    })
+
+  })
+
+})
